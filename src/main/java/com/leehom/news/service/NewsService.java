c9e15a27504ab27c1bfe7fa0e2cae7bed0e35dc7
lee0720 <945920116@qq.com>
2018-12-24 19:25:19 +0800

    2018-12-24

---
+++ src/main/java/com/leehom/news/service/NewsService.java
@@ -0,0 +1,11 @@
+package com.leehom.news.service;
+
+import com.leehom.news.dao.NewsDao;
+
+public class NewsService {
+    private NewsDao newsDao;
+
+
+
+
+}
---
+++ src/main/java/com/leehom/news/service/impl/NewsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.leehom.news.service.impl;
+
+import com.leehom.news.service.NewsService;
+
+public class NewsServiceImpl extends NewsService {
+
+}
