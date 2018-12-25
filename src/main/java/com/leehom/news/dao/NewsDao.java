e92554d625f2471d9b72ce60ee49f6287474af7c
lee0720 <945920116@qq.com>
2018-12-25 11:38:33 +0800

    首页导航栏查询

--- src/main/java/com/leehom/news/dao/NewsDao.java
+++ src/main/java/com/leehom/news/dao/NewsDao.java
@@ -1,4 +1,17 @@
 package com.leehom.news.dao;
 
+import com.leehom.news.po.News;
+
+import java.util.List;
+
 public interface NewsDao {
+
+    List<News> selectNewsByDetailId(Integer detailId);
+
+    /**
+     * 首页体育/娱乐/财经相关内容的显示
+     * @param typeId
+     * @return
+     */
+    List<News> selectNewsByTypeId(Integer typeId);
 }
