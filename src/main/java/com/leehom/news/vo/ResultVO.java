9909a9f966399722b369f87fc3bee1534f494f16
leehom <945920116@qq.com>
2018-12-26 21:18:03 +0800

    添加vo文件夹,vo存储返回给页面的对象

--- src/main/java/com/leehom/news/vo/ResultVO.java
+++ src/main/java/com/leehom/news/vo/ResultVO.java
@@ -1,4 +1,14 @@
 package com.leehom.news.vo;
 
-public class ResultVO {
+import lombok.Data;
+
+@Data
+public class ResultVO<T> {
+
+    private Integer code;
+
+    private String message = "";
+
+    private T data;
+
 }
