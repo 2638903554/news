fe2a03ce21346875139b836b108a6702cfda6810
leehom <945920116@qq.com>
2018-12-26 21:17:23 +0800

    添加exception 异常类文件夹

--- src/main/java/com/leehom/news/exception/NewsException.java
+++ src/main/java/com/leehom/news/exception/NewsException.java
@@ -1,4 +1,19 @@
 package com.leehom.news.exception;
 
-public class NewsException {
+import com.leehom.news.enums.ResultEnum;
+
+public class NewsException extends RuntimeException {
+
+    private Integer code;
+
+    public NewsException(ResultEnum resultEnum){
+        super(resultEnum.getMessage());
+        this.code = resultEnum.getCode();
+    }
+
+    public NewsException(Integer code,String message){
+        super(message);
+        this.code = code;
+    }
+
 }
